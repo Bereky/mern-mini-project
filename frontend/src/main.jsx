999f10b99a47edce00eed8735876cb3fecbb4cd9
unknown <bereketlemma4@gmail.com>
2025-03-04 03:15:06 +0300

    fix: update toaster position

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { Route, BrowserRouter as Router, Routes } from 'react-router'
-import { Toaster } from 'react-hot-toast'
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { Route, BrowserRouter as Router, Routes } from "react-router";
+import { Toaster } from "react-hot-toast";
 
-import './index.css'
-import App from './app/App'
+import "./index.css";
+import App from "./app/App";
 import { store } from "./redux/app/store";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <Router>
       <Routes>
@@ -16,9 +16,9 @@ createRoot(document.getElementById('root')).render(
     </Router>
     <Toaster
       toastId="default"
-      position="top-right"
+      position="top-center"
       reverseOrder={false}
       toastOptions={{ className: "text-sm", duration: 3000 }}
     />
   </Provider>
-)
+);
