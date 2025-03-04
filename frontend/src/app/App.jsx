ca52f017fd099e812a991619b1ed5143ade9847f
unknown <bereketlemma4@gmail.com>
2025-03-04 03:58:25 +0300

    feat: add router for invalid routes

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router";
+
 import Login from "./login/page";
 import Register from "./register/page";
 import Layout from "./layout/layout";
+import PageNotFound from "../components/page-not-found";
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/" element={<Layout />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
