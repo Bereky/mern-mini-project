7106b4c7706429508f5ffe5afd454820e05354e7
unknown <bereketlemma4@gmail.com>
2025-03-04 03:17:18 +0300

    chore: cleaning

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,10 +1,9 @@
-import { Route, Routes, useLocation } from "react-router";
+import { Route, Routes } from "react-router";
 import Login from "./login/page";
 import Register from "./register/page";
 import Layout from "./layout/layout";
 
 const App = () => {
-  const location = useLocation();
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
